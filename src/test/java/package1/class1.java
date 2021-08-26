a4e342811f9ff88ae53894a168f400ba2dcc49ad
karshjarak <karshi555@gmail.com>
2021-08-26 10:05:14 +0530

    aaaaaaaaaa

---
+++ src/test/java/package1/class1.java
@@ -0,0 +1,49 @@
+package package1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class class1 {
+
+	public static WebDriver driver;
+
+	public String browser = "chrome";
+
+	@BeforeSuite
+	public void setUp() {
+
+		if (browser.equals("chrome")) {
+
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+
+		} else if (browser.equals("firefox")) {
+
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+
+		}
+
+		driver.manage().window().maximize();
+
+	}
+
+	@AfterSuite
+	public void tearDown() {
+
+		driver.quit();
+
+	}
+}
---
+++ src/test/java/package1/class2.java
@@ -0,0 +1,18 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class class2 extends class1{
+
+	
+	
+	@Test
+	public void testng() {
+		
+		driver.get("http:///www.facebook.com");
+	}
+	
+	
+	
+
+}
